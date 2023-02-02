b9ab9a52f547c906dab8e7685e56fb2b62ba1f78
Huang Anya <huang.anya@activate.sg>
2023-02-02 08:09:19 +0800

    add some navigation between host and remote

--- apps/remote/src/app/app.tsx
+++ apps/remote/src/app/app.tsx
@@ -1,10 +1,16 @@
-import { Typography } from '@mui/material';
+import { Link, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import { AppRoutes } from '../routes/app-routes';
 
 export function App() {
+  const navigate = useNavigate();
   return (
     <>
-      <Typography variant="h1">It is the Remote App </Typography>
+      <Typography variant="h5">It is the Remote App </Typography>
+      <Link onClick={() => navigate('/')}>
+        Click here to root(either remote's root or host's root, depends on where
+        it's rendered)
+      </Link>
       <AppRoutes />
     </>
   );
--- libs/app-shared/app-manager/providers/theme-provider.tsx
+++ libs/app-shared/app-manager/providers/theme-provider.tsx
@@ -2,13 +2,11 @@ import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
 import { palette, typography, components } from '../../constants/theme';
 
-export const theme = responsiveFontSizes(
-  createTheme({
-    palette,
-    typography,
-    components,
-  })
-);
+export const theme = createTheme({
+  palette,
+  typography,
+  components,
+});
 
 type AppThemeProviderProp = { children?: React.ReactElement };
 
