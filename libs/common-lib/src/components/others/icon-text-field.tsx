97e5b050cee1979c874d8b1da7c7e78d1c09190e
Huang Anya <huang.anya@activate.sg>
2023-02-03 13:48:50 +0800

    add component of IconTextField

---
+++ libs/common-lib/src/components/others/icon-text-field.tsx
@@ -0,0 +1,26 @@
+import { TextField, InputAdornment, TextFieldProps } from '@mui/material';
+
+type IconTextFieldProps = {
+  iconStart?: React.ReactElement;
+  iconEnd?: React.ReactElement;
+} & TextFieldProps;
+
+export const IconTextField = ({
+  iconStart,
+  iconEnd,
+  ...props
+}: IconTextFieldProps) => {
+  return (
+    <TextField
+      {...props}
+      InputProps={{
+        startAdornment: iconStart ? (
+          <InputAdornment position="start">{iconStart}</InputAdornment>
+        ) : null,
+        endAdornment: iconEnd ? (
+          <InputAdornment position="end">{iconEnd}</InputAdornment>
+        ) : null,
+      }}
+    />
+  );
+};
